46a1984ca38052469dc9b10f1afa75bab078ab62
PierreVir <pierre.virama@gmail.com>
2024-03-25 13:59:03 +0100

    first step portfolio.js: change second avatar not finished

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+
+const ScndImg = document.querySelector(".avatar");
+
+ScndImg.addEventListener('click', function (){
+    imgScnd.src = "/image/avatar.svg";
+    imgScnd.alt = "second avatar on click";
+})
+
