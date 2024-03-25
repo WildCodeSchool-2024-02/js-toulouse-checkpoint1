43ba73e7c40f8d9d36a96b98532120c90daee629
ArthurHtbk <arthur.heurtebise@gmail.com>
2024-03-25 16:07:10 +0100

    JS 3 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,13 +1,16 @@
 const avatar = document.querySelector("#avatar");
 const firstname = document.querySelector("#firstname");
 const button = document.querySelector(".color-change");
+const description = document.querySelector(".description");
 
 avatar.addEventListener("click", () => {
   avatar.src = "image/avatar.svg";
 });
 
 button.addEventListener("click", () => {
+  const color = prompt("Enter your favorite color");
   const username = prompt("What's your name");
   firstname.innerText = username;
   firstname.style.color = "white";
+  description.style.backgroundColor = color;
 });
