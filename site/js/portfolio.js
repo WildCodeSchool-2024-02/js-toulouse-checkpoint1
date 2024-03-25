fca3f620a295e416baf0dcabd9bfd33667334160
Modrum <tristanlaroye@hotmail.com>
2024-03-25 12:53:35 +0100

    finish all optionnal js in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -13,9 +13,39 @@ button.addEventListener("click", () => {
   const span = document.querySelector(".description span");
   span.style.color = "white";
   const descriptionBoxes = document.querySelectorAll(".description");
-  const hFour = document.querySelectorAll("h4");
+  const hFour = document.querySelectorAll("h4,a");
+
   const userColorChoice = prompt("Enter a color");
   descriptionBoxes.forEach((element) => (element.style.backgroundColor = userColorChoice));
   hFour.forEach((element) => (element.style.color = userColorChoice));
   span.innerText = prompt("what's your name ?");
+});
+
+const buttonFrontLi = document.createElement("button");
+buttonFrontLi.innerText = "Modify";
+const skillsContainer = document.querySelector(".skills-container");
+const frontBox = skillsContainer.lastElementChild;
+frontBox.appendChild(buttonFrontLi);
+
+buttonFrontLi.addEventListener("click", () => {
+  const arraySkills = ["VSCode", "Github", "Terminal"];
+  const frontDevToolsLi = document.querySelectorAll("#front-dev-tools li");
+  for (let i = 0; i < arraySkills.length; i++) {
+    frontDevToolsLi[i].innerText = arraySkills[i];
+  }
+});
+
+const backBox = skillsContainer.firstElementChild;
+const input = document.createElement("input");
+input.type = "text";
+const buttonAddSkills = document.createElement("button");
+buttonAddSkills.innerText = "Add Skill";
+backBox.appendChild(input);
+backBox.appendChild(buttonAddSkills);
+
+buttonAddSkills.addEventListener("click", () => {
+  const ul = document.querySelector("#back-dev-tools");
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  ul.appendChild(li);
 });
