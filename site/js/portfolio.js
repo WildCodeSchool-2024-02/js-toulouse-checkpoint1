9266cf5f0a28d46feb11d9852bff8ef8989e530c
superwann32 <142965165+superwann32@users.noreply.github.com>
2024-03-25 12:22:01 +0100

    site step css ok

---
+++ site/js/portfolio.js
@@ -0,0 +1,9 @@
+function changeAvatar(avatar) {
+    const img = document.getElementById(avatar)
+    document.addEventListener("click").classList.toggle()
+    // if (img.src == "image/avatar-bis.png") {
+    //     img.src = "image/avatar.png"
+    // } else {
+    //     img.src = "image/avatar-bis.png"
+    // }
+}
