7f1af2d82c6e0f1ca9e926ada13ad0d7a94dbb64
Modrum <tristanlaroye@hotmail.com>
2024-03-25 10:40:11 +0100

    finish algo 3 in part 1

--- algo/3/findAdults.js
+++ algo/3/findAdults.js
@@ -25,8 +25,33 @@ const persons = [
 
 */
 
+const persons = [
+  { name: "Mary Poppins", age: 32, sex: "female" },
+  { name: "Tony Stark", age: 36, sex: "male" },
+  { name: "John Doe", age: 12, sex: "male" },
+  { name: "Jane Doe", age: 6, sex: "female" },
+  { name: "Maggie Simpson", age: 2, sex: "female" },
+  { name: "Leonardo", age: 92, sex: "male" },
+  { name: "Carla Moreau", age: 24, sex: "female" },
+  { name: "Mickael Jackson", age: 17, sex: "male" },
+  { name: "Penelope Milena", age: 27, sex: "female" },
+  { name: "Homer Simpson", age: 45, sex: "male" },
+  { name: "Leonardo Di Caprio", age: 42, sex: "male" },
+  { name: "Carla Bruni", age: 54, sex: "female" },
+  { name: "Lisa Simpson", age: 13, sex: "female" },
+  { name: "Millie Bobby Brown", age: 15, sex: "female" },
+  { name: "Penelope Cruz", age: 47, sex: "female" },
+];
+
 function findAdults(persons) {
-  // Your code here !
+  const adultOnly = persons.filter((elem) => elem["age"] >= 18);
+  const maleAndFemale = [
+    [...adultOnly.filter((elem) => elem["sex"] === "female")],
+    [...adultOnly.filter((elem) => elem["sex"] === "male")],
+  ];
+  return maleAndFemale;
 }
+
+console.log(findAdults(persons));
 
 module.exports = findAdults;
