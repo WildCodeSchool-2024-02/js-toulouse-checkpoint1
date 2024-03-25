c3d90182f861049aa2e246f0d1a1fcf314ecb045
ArthurHtbk <arthur.heurtebise@gmail.com>
2024-03-25 16:13:26 +0100

    JS 4 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,6 +2,8 @@ const avatar = document.querySelector("#avatar");
 const firstname = document.querySelector("#firstname");
 const button = document.querySelector(".color-change");
 const description = document.querySelector(".description");
+const pinkBg = document.querySelectorAll(".pink-bg");
+const pinkText = document.querySelectorAll(".pink-text");
 
 avatar.addEventListener("click", () => {
   avatar.src = "image/avatar.svg";
@@ -13,4 +15,12 @@ button.addEventListener("click", () => {
   firstname.innerText = username;
   firstname.style.color = "white";
   description.style.backgroundColor = color;
+
+  for (let i = 0; i < pinkBg.length; i++) {
+    pinkBg[i].style.backgroundColor = color;
+  }
+
+  for (let i = 0; i < pinkText.length; i++) {
+    pinkText[i].style.color = color;
+  }
 });
