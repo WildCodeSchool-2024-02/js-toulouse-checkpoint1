a50f54e11fa3cb3c05ab77477ef552ea52b73b03
Romain <97295039+RomainRG31@users.noreply.github.com>
2024-03-25 12:48:03 +0100

    finished second part of site

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,2 +1,19 @@
 const avatar = document.querySelector(".avatar-img");
 avatar.addEventListener("click", () => (avatar.src = "./image/avatar.svg"));
+
+const buttonChanges = document.querySelector(".btn-changes-name");
+let name = document.querySelector("#firstname");
+let backgrounds = document.querySelectorAll(".pink-bg");
+let h4 = document.querySelectorAll("h4");
+
+buttonChanges.addEventListener("click", () => {
+  let newColor = prompt("Enter the new color");
+  let newName = prompt("Enter your name");
+  backgrounds.forEach((background) => {
+    background.style.backgroundColor = newColor;
+  });
+  h4.forEach((title) => {
+    title.style.color = newColor;
+  });
+  name.textContent = newName;
+});
