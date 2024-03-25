823143d0899cc42c37789d858e5187346066da98
ArthurHtbk <arthur.heurtebise@gmail.com>
2024-03-25 16:00:37 +0100

    JS 2 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,13 @@
 const avatar = document.querySelector("#avatar");
+const firstname = document.querySelector("#firstname");
+const button = document.querySelector(".color-change");
 
 avatar.addEventListener("click", () => {
   avatar.src = "image/avatar.svg";
+});
+
+button.addEventListener("click", () => {
+  const username = prompt("What's your name");
+  firstname.innerText = username;
+  firstname.style.color = "white";
 });
